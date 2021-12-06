d7fa503624d10332479629c3228e401da9a40b5c
Crystal9202 <jpkoq18@gmail.com>
2021-12-06 19:56:55 +0800

    權限再細分, 加入管理員

--- Hotel/__init__.py
+++ Hotel/__init__.py
@@ -1,4 +1,4 @@
-
+import click
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -23,11 +23,41 @@ def create_app():   #工廠函數
     db.init_app(app)
     jwt.init_app(app)
     migrate=Migrate(app,db)
-
+    register_commands(app)
 
     api.add_resource(UserList ,"/users")
     api.add_resource(User, '/user/<string:username>')
     api.add_resource(Tweet,"/tweets/<string:username>")
     return app
+
+
+def register_commands(app):
+    @app.cli.command()
+    @click.option('--username' ,prompt=True , help ='The username used to login')
+    @click.option('--password' ,prompt=True ,hide_input=True ,confirmation_prompt=True ,help='The password used to login.')
+    @click.option('--email' ,prompt=True ,help ='The email is required')
+    def admin(username , password,email):
+        try:
+            user=UserModel.get_by_name(username)
+            if user is not None:  #在這裡沒辦法寫 if user:
+                click.echo("username already exists.")
+            else:
+                admin = UserModel.query.filter(UserModel.from_admin == True).first()
+                if admin:   #如果數據庫中已經有管理員紀錄就更新用戶名和密碼
+                    click.echo('The administrator already exists, updating...')
+                    admin.username = username
+                    admin.set_password(password)
+                    admin.email = email
+                else:  #否則創建新的管理員紀錄
+                    click.echo('Creating the temporary adminstrator account...')
+                    admin=UserModel(username = username , email = email ,from_admin=True)
+                    admin.set_password(password)
+                    db.session.add(admin)
+                db.session.commit()
+                click.echo('Done')
+        except:
+            click.echo('Database connection failed.')
+
+            
 
 
