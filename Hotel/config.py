4606c313eee2ed4b80041cb0e9522d69b049acf9
Crystal9202 <jpkoq18@gmail.com>
2021-12-14 20:26:54 +0800

    第六次代碼重構

--- Hotel/config.py
+++ Hotel/config.py
@@ -9,3 +9,25 @@ class Config:
     JWT_AUTH_URL_RULE = "/auth/login"
     SECRET_KEY="flask123"
     JWT_AUTH_HEADER_PREFIX = "FLASK"
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI= f"mysql+pymysql://root:{os.getenv('PASSWORD')}@localhost:3306/hotel"
+    
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI" ,'sqlite:///data.db')
+
+
+app_config = {
+    "testing" : TestingConfig , 
+    "development" : DevelopmentConfig ,
+    "production" : ProductionConfig
+}
+
+
+
+
